9542ce8f8fa5387aa64a7812a7581e6b728c45a4
sergioguillen03 <84127494+sergioguillen03@users.noreply.github.com>
2021-10-26 21:08:38 -0700

    Add files via upload

---
+++ MethodOverloading.java
@@ -0,0 +1,26 @@
+package Overload;
+
+public class MethodOverloading {
+	 public static void main(String[] args) {
+		   //create instance here
+		 MethodOverloading  om = new MethodOverloading ();
+		   om.print();
+
+		   om.print("second print method");
+
+		   om.print("second", "third");
+		 }
+
+		 //overload methods here
+		 public void print(){
+		System.out.println("original print method.");
+		 }
+
+		 public void print(String str){
+		System.out.println(str);
+		 }
+
+		 public void print(String str, String str2){
+		System.out.println(str + " " + str2);
+		 }
+		}
