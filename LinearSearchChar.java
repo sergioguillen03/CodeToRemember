a427421aa3250ae200e90eac0fbc5d7525d647d1
sergioguillen03 <84127494+sergioguillen03@users.noreply.github.com>
2021-10-26 20:55:28 -0700

    Add files via upload

---
+++ LinearSearchChar.java
@@ -0,0 +1,58 @@
+package algorithms;
+
+public class LinearSearchChar {
+	public static void main(String[] args) {
+
+		//create some dummy data for our method
+
+		   char letter = 'm';
+
+		   char[] letters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+
+		's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+		 
+
+		   //call your methods here
+
+		   LinearSearchChar ls = new LinearSearchChar();
+
+		   System.out.println(ls.findIndex(letter, letters));
+
+		}
+
+		 
+
+		public int findIndex(char target, char[] data) {
+
+		int result = -1;
+
+		 
+
+		//loop through the data
+
+		for (int i = 0; i < data.length; i++) {
+
+		 
+
+		char temp = data[i];
+
+		 
+
+		//test current element against target
+
+		if (temp == target) {
+
+		return i;
+
+		}
+
+		}
+
+		 
+
+		return result;
+
+		}
+
+		}
